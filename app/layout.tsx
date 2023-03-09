1a4ba6e68ade3732cec61dcda94548bddae6ae86
NAO <nao2bs@gmail.com>
2023-03-09 15:41:41 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,19 +4,9 @@ import '../styles/global.css'
 import '../styles/syntax-coloring.css'
 import styles from '../styles/shared.module.css'
 
-<<<<<<< HEAD
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang="ja" prefix="og: https://ogp.me/ns#">
     <body>
-=======
-const RootLayout = ({
-  children,
-}: {
-  children: React.ReactNode,
-}) => (
-  <html lang="ja" prefix="og: https://ogp.me/ns#" className='dark'>
-    <body className='dark:bg-gray-800'>
->>>>>>> 8b08bed3272b23adcae757599204395c0bde3dd3
       <div className={styles.container}>
         <Header />
         <div className={styles.content}>{children}</div>
